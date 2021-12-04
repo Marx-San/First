e7fe83dfdaf23b36869d45612cbb4a9c84298cb2
Marx-San <95501057+Marx-San@users.noreply.github.com>
2021-12-05 00:18:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,64 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+//api
+console.log("New");
+
+var myHeaders = new Headers();
+myHeaders.append("x-access-token", "goldapi-xjk0natkwrpbh8a-io");
+myHeaders.append("Content-Type", "application/json");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+const api_url = 'https://www.goldapi.io/api/XAU/INR';
+
+let count = 0;
+
+export default class App extends React.Component{
+
+  state = {
+    loading:true,
+    high_price:null,
+    low_price:null
+  }
+
+  async componentDidMount(){
+    setInterval(async () => {
+      console.log('1');
+      var response =  await fetch(api_url,requestOptions);
+      var data = await response.json();
+
+      this.setState({high_price:data.high_price});
+      this.setState({low_price:data.low_price});
+   }, 5000); // 5 sec refresh
+  }
+  
+  async componentWillUnmount() {
+    clearInterval(this.timer);
+    this.timer = null; // here...
+  }
+
+  render(){
+  return(
+    <View style={styles.container}>
+      <Text>high {this.state.high_price}</Text>
+      <Text>low {this.state.low_price}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
